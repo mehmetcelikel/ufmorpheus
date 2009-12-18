da304f60ff07edfd8f3324a3ecffb7bc8fbfe57a
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2009-12-18 21:40:25 +0000

    created main .. next is data parser

--- trunk/qre/ActionState.py
+++ trunk/qre/ActionState.py
@@ -14,7 +14,7 @@ class ActionState:
 		It requires that you supply the (key,value) (key,type) and action tables
 
 	"""
-	def __init__(self, _action_list, _kv_hash, _kt_hash, _action_hash ):
+	def __init__(self, _action_list, _kv_hash, _kt_hash, _user_hash ):
 		self.page = None # The current response page and dom source
 		self.user_agent = None # Header for user agent and browser info
 		self.cookie = CookieJar()# Cookie object
@@ -22,13 +22,13 @@ class ActionState:
 		
 		self.kv_hash = _kv_hash.copy() # Make a copy of the hash table
 		self.kt_hash = _kt_hash.copy() # Make a copy of the hash table
-		self.action_hash = _action_hash.copy() # Copy hash table
+		self.user_hash = _user_hash.copy() # Copy hash table
 
 		self.action_list = _action_list.copy() # The list of actions 
 		#self.iter = action_list.__iter__() # The action iterator
 
 
-	def run():
+	def run(self):
 		""" This function call the do function on all the items im action_list"""
 		map(lambda a: a.do(self), action_list) # State passed in is mutable
 
--- trunk/qre/main.py
+++ trunk/qre/main.py
@@ -4,7 +4,17 @@ This class runs the code
 
 __authors___ = ['"Christan Earl Grant" <cgrant@cise.ufl.edu>']
 
-def main():
+import lxml
+from lxml import etree
+
+import psycopg2
+import sys
+
+connect_string = "dbname='%(db)s' user='%(user)s' host='%(server)s' password='%(pwd)s'"
+__connect_params = {'server': "babylon.cise.ufl.edu", 'user' : "morpheus3",\
+										 'pwd' : "crimson03.sql", 'db' : "Morpheus3DB"}
+
+def main(argv):
 """Run the Executor script
 	
 	Iterate once over the script xml
@@ -20,5 +30,57 @@ def main():
 	Initialize state with the created data structures
 	Call state.run()
 """
-	pass
+	id = sys.argv[-1]	
+
+	try:
+		connection = psycopg2.connect(connect_string % __connect_params)
+	except:
+		print ("Connection to database failed")
+		return None
+	
+	q = "SELECT code FROM qrm WHERE qrm.id = {id}" % {'id':id}
+	
+	cursor = connection.cursor()
+	cursor.execute(q)
+	result = cursor.fetchall()
+	code = result[0][0]
+	
+	assert(len(code) > 0)
+	
+	root = etree.fromstring(code)
+
+	action_list = []
+	kv_hash = {}	
+	kt_hash = {}
+	user_hash = {}
+
+	# Traversal of code script
+	for child in root:
+		if child.tag == 'actiondata':
+			kv_hash, kt_hash = ActionDataParser(child)
+
+		elif child.tag == 'userdata':
+			user_hash = UserDataParser(child)
+
+		elif child.tag == 'starturl':
+			action_list.insert(0,URLAction(child))
+
+		elif child.tag == 'sequence':
+			for ao in child:
+				if ao.tag == 'constantlink'
+					action_list.append(URLAction(ao))
+
+				elif ao.tag == 'link'
+					action_list.append(LinkAction(ao))
+
+				elif ao.tag == 'highlight'
+					action_list.append(HighlightAction(ao))
+
+				elif ao.tag == 'form'
+					action_list.append(FormAction(ao))
+	
+	state = ActionState(action_list, kv_hash, kt_hash, user_hash)
+	state.run()
+
+	return state
 
