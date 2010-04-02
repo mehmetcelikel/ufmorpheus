0cc3ee7daabd01a5002dba8926ba8ce88d13b168
gycordero <gycordero@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-04-02 03:03:03 +0000

    Removing importing of classes and properties from file. Including them in the same file in the ssq

--- trunk/OWLMorpheus/src/filesystem/FTPUploadFile.java
+++ trunk/OWLMorpheus/src/filesystem/FTPUploadFile.java
@@ -19,7 +19,7 @@ import com.zehon.sftp.SFTP;
 		 */
 		public static void transferZion(String file, String destination) {
 			
-			/*
+			
 			//Disable Printing to the screen
 			PrintStream printStreamOriginal=System.out; //Standard output
 			PrintStream printStreamError=System.err;	//Standard error
@@ -31,7 +31,7 @@ import com.zehon.sftp.SFTP;
 	        	public void write(int b) {
 				}
 			}));
-			*/
+			
 			
 			String host = "zion.cise.ufl.edu";
 			String username = "researcher";
@@ -50,11 +50,11 @@ import com.zehon.sftp.SFTP;
 				e.printStackTrace();
 			}
 			
-			/*
+			
 			//Enabling printing to the screen again
 			System.setOut(printStreamOriginal);
 			System.setErr(printStreamError);
-			*/
+			
 			
 		}
 		
