a116d2f17663be1cf9f508e7566bb704451b4acd
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-04-16 19:02:31 +0000

    added new stuff

---
+++ userstudy/ssq/index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+		<script type="text/javascript" src="http://dev.jquery.com/view/trunk/plugins/autocomplete/jquery.autocomplete.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.js"></script>
+		<link type="text/css" rel="stylesheet" href="style.css" />
+		<script type="text/javascript" rel="javascript" src="logic.js"> </script>
+		<script>
+			$(document).ready(function(){
+				init();
+			});
+		</script>
+	</head>
+	<body>
+		<form method="post">
+			<h3> Question 1 </h3>
+			<div id="question">
+				<h1>
+					<div index="0" class="term">What <input type="text" name="term0" size="10" class="catbox" /></div> 
+					<div index="1" class="term">size <input type="text" name="term1" size="10" class="catbox" /> </div> 
+					<div index="2" class="term">tires <input type="text" name="term2" size="10" class="catbox" /></div> 
+					<div index="3" class="term">does <input type="text" name="term3" size="10" class="catbox" /></div> 
+					<div index="4" class="term">a <input type="text" name="term4" size="10" class="catbox" /></div> 
+					<div index="5" class="term">97 <input type="text" name="term5" size="10" class="catbox" /></div> 
+					<div index="6" class="term">Toyota <input type="text" name="term6" size="10" class="catbox" /></div> 
+					<div index="7" class="term">Camry <input type="text" name="term7" size="10" class="catbox" /></div> 
+					<div index="8" class="term">need? <input type="text" name="term8" size="10" class="catbox" /></div> 
+				</h1>
+			</div>
+		
+			<div id="drop-panels">
+		
+				<div id="input-panel" class="droppable">
+					<p class="panel-name">Input Terms</p>
+				</div>
+		
+				<div id="output-panel" class="droppable">
+					<p class="panel-name">Output Terms</p>
+				</div>
+		
+				<div class="clear"></div>
+			</div>
+			<input type="submit" name="submitbutton" id="submitbutton" value="Next=>" />
+		</form>
+	</body>
+</html>
--- userstudy/ssq/logic.js
+++ userstudy/ssq/logic.js
@@ -3,14 +3,36 @@
 */
 
 function init(){
-	YUI().use('dd-drag', function(Y) {
-		// Selector of the node to make draggable
-		var dd = new Y.DD.Drag({
-			node: '#demo'
-		});
+	var acdata = 'Hello This is just some sample data so that I can let people use candom categories there are more automobiles and a ton of other categories for people to uses Automobiles Music Movies Airplanes Engines Car'.split(' ');
+	
+	// Give the input boxes auto complete power
+	$('.catbox').autocomplete(acdata);
+	
+	// Hide input boxes by default
+	$('.catbox').hide();
+	
+	$('.term').dblclick(function () {
+		//$('.catbox').slideToggle('slow');
+		$(this).find('.catbox').slideToggle('fast');
 	});
-
-	YUI().use('dd-constrain', function(Y) {
+	
+	// Make terms dragable
+	$('.term').draggable({
+		revert: 'invalid', // when not dropped, the item will revert back to its initial position
 	});
-
+	
+	// Logic for when the terms are dropped
+	$('.droppable').droppable({
+		drop: function(event, ui) {
+			$(this).find('p').html('Dropped!');
+			//$(this).addClass('ui-state-highlight')
+		},
+		out: function(event, ui) {
+			$(this).find('p').html('Removed');
+		},
+		activeClass: 'activeDiv'
+	});
+		
 }
+
+
