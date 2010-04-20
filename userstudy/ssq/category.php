bfef037f9134b4c2a2cb0eb8a540ba04d71b11eb
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-04-20 18:57:18 +0000

    added working auto complete

--- userstudy/ssq/category.php
+++ userstudy/ssq/category.php
@@ -1,20 +1,35 @@
 #!/usr/local/bin/php
-<?php
 
-	if ($_GET['q'] != null){
-		$query = $_GET['q'];
+<?php
+	$MAX_RESULTS = 7;
+	$conn  = pg_connect('user=morpheus3 host=babylon.cise.ufl.edu dbname=sdb password=crimson03.sql');
+	
+	if (!$conn) { 
+		echo "Connection failed";
 		
-		echo $query;
-		echo "christan";
-		echo "automobiles";
-		echo "christan";		
+		exit;
+	}
+	
+	if ($_GET['q'] != null){
+		$q = $_GET['q'];
+		
+		$query = 'SELECT distinct lex FROM nodes WHERE lex ILIKE '.
+								'\'http://zion.cise.ufl.edu/ontology/classes#'.$q.'%\'';
+		
+		$result = pg_query($conn, $query);
+		$counter = 0;
+		while($row = pg_fetch_array($result, NULL, PGSQL_NUM)){
+			
+			print substr(strrchr($row[0], "#"), 1);
+			print "\n";
+			$counter += 1;
+			if ($counter == MAX_RESULTS) break;
+		}
+
 		
 	}
 	else{
 		// Do nothing or return error
-		echo "1christans";
-		echo "2automobilses";
-		echo "3christasn";	
 	}
 
 ?>
