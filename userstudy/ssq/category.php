5a9c4074248e8cc6271f87aac0e3323d815762c6
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-04-20 17:46:14 +0000

    latest

---
+++ userstudy/ssq/category.php
@@ -0,0 +1,20 @@
+#!/usr/local/bin/php
+<?php
+
+	if ($_GET['q'] != null){
+		$query = $_GET['q'];
+		
+		echo $query;
+		echo "christan";
+		echo "automobiles";
+		echo "christan";		
+		
+	}
+	else{
+		// Do nothing or return error
+		echo "1christans";
+		echo "2automobilses";
+		echo "3christasn";	
+	}
+
+?>
