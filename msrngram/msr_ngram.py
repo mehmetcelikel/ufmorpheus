0151ac3db7b357cb9e1516452fc808c90425c1a9
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-05-11 22:24:20 +0000

    check point...

--- msrngram/msr_ngram.py
+++ msrngram/msr_ngram.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 
-
 import SOAPpy
 from SOAPpy import WSDL
+
+WSDLFILE = 'http://web-ngram.research.microsoft.com/Lookup.svc/mex?wsdl'
+
+def getguid(file_name='guid.rc'):
+	"""Pulls GUID from a local file from parameter. 
+
+	The GUID is on first line of the file.
+		
+	"""
+	f = open(file_name)
+	g = f.readline()
+	f.close()
+	return g.strip()
+
+
+_server = WSDL.Proxy(WSDLFILE)
+def getModels(param1, param2):
+	"""The get models thing"""
+	results = _server.GetModels(param1, param2)
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
+	print getModels('Christan', 'Grant')
