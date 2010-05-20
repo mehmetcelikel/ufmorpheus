23947b245a9e842d6cc261c0b11f88fdd03d64a6
christangrant <christangrant@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-05-20 16:17:26 +0000

    Added an install package

--- msrngram/setup.py
+++ msrngram/setup.py
@@ -1,3 +1,7 @@
 from distutils.core import setup
-setup(name = 'msrngram', version = '0.5 ', \
+
+setup(name = 'msrngram', version = '0.5 ', 
+				description = 'python interface to using the MSR ngram service',
+				author = 'Christan Grant',
+				author_email = 'christangrant@gmail.com',
 				py_modules = ['msrngram', 'payload'])
