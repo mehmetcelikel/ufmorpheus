6492e032004d11150a38037f7d3d564a388b655e
landersben <landersben@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-07-29 22:05:33 +0000

    Wrote logic for ssqbuilder method. Unsure what to do with individualid and queryid.

--- trunk/ssqbuilder/ssqbuilder.py
+++ trunk/ssqbuilder/ssqbuilder.py
@@ -1,18 +1,63 @@
 #!/usr/bin/python
 
-import lxml, lxml.etree
+import lxml
 import psycopg2
 
 
+def etree_with_text(tag, text, **kwargs):
+        """ Small function to construct an etree with a text field """
+	etree = lxml.etree.Element(tag, **kwargs)
+	etree.text = text
+	return etree
+
 def ssqbuilder(queryid):
 	""" Takes a query id and returns the ssq representation """
-	ssqinfo = get_ssq_info(queryid)
+	ssqinfo_list = get_ssq_info(queryid)
+	
+	## Used test case found at: http://pastebin.com/i1MKxmfM
+	##ssqinfo_list = [{'contextname': 'What', 'realm': 'Automotive', 'name': 'Sedan', 'querystring': 'Where can I buy an engine for a Toyota Camry V6?', 'individualid': 1055, 'queryid': 595, 'io': 'input', 'phrasestring': 'Toyota Camry V6'}, {'contextname': 'What', 'realm': 'Automotive', 'name': 'Manufacturer', 'querystring': 'Where can I buy an engine for a Toyota Camry V6?', 'individualid': 1056, 'queryid': 595, 'io': 'input', 'phrasestring': 'Toyota'}, {'contextname': 'What', 'realm': 'Automotive', 'name': 'Model', 'querystring': 'Where can I buy an engine for a Toyota Camry V6?', 'individualid': 1057, 'queryid': 595, 'io': 'input', 'phrasestring': 'Camry V6'}, {'contextname': 'What', 'realm': 'Automotive', 'name': 'Part', 'querystring': 'Where can I buy an engine for a Toyota Camry V6?', 'individualid': 1058, 'queryid': 595, 'io': 'output', 'phrasestring': 'engine'}]
 
-	# TODO - Add logic here for building the ssq stuff
+	## Results of test case:
+	##<ssq>
+	##  <realm>Automotive</realm>
+	##  <query>Where can I buy an engine for a Toyota Camry V6?</query>
+	##  <input_list>
+	##    <input dataclass="Sedan" type="What">Toyota Camry V6</input>
+	##    <input dataclass="Manufacturer" type="What">Toyota</input>
+	##    <input dataclass="Model" type="What">Camry V6</input>
+	##  </input_list>
+	##  <output_list>
+	##    <output dataclass="Part" type="What">engine</output>
+	##  </output_list>
+	##</ssq>
 
+	
+	# Realm and query should be the same for all
+	# Construct ssq
+	ssq_etree = lxml.etree.Element('ssq')
+	ssq_etree.append(etree_with_text(tag='realm', text=ssqinfo_list[0]['realm']))
+	ssq_etree.append(etree_with_text(tag='query', text=ssqinfo_list[0]['querystring']))
+
+	input_etree = lxml.etree.Element('input_list')
+	output_etree = lxml.etree.Element('output_list')
+	for ssq in ssqinfo_list:
+		if ssq['io'].lower() == 'input':
+			input_etree.append(etree_with_text('input', ssq['phrasestring'],
+							   **{'type':ssq['contextname'], 'dataclass':ssq['name']}))
+		elif ssq['io'].lower() == 'output':
+			output_etree.append(etree_with_text('output', ssq['phrasestring'],
+							   **{'type':ssq['contextname'], 'dataclass':ssq['name']}))
+		else:
+			pass # Throw an exception?
+
+	ssq_etree.append(input_etree)
+	ssq_etree.append(output_etree)
+
+	# Unsure what to do with individualid and queryid
+	
 	put_ssq_script(queryid, ssqstring) # The ssqstring is the prettyprinted xml 
 	
-	return ssqstring
+	return lxml.etree.tostring(ssq_etree, pretty_print=True)
 
 
 __connect_string = "dbname='%(db)s' user='%(user)s' host='%(server)s'	password='%(pwd)s'"
