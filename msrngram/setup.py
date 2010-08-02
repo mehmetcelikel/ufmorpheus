4d156c00c53e6a6b15d9321e4f42a52d86e7598a
landersben <landersben@199c1a6e-df8e-11de-b2e4-dbb720ae924e>
2010-08-02 02:53:03 +0000

    Generated documentation using PyDoc. Modified two files to accommodate PyDoc.

--- msrngram/setup.py
+++ msrngram/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 
-setup(name = 'msrngram', version = '0.5 ', 
-				description = 'python interface to using the MSR ngram service',
-				author = 'Christan Grant',
-				author_email = 'christangrant@gmail.com',
-				py_modules = ['msrngram', 'payload'])
+if __name__ == "__main__":
+	setup(name = 'msrngram', version = '0.5 ', 
+					description = 'python interface to using the MSR ngram service',
+					author = 'Christan Grant',
+					author_email = 'christangrant@gmail.com',
+					py_modules = ['msrngram', 'payload'])
